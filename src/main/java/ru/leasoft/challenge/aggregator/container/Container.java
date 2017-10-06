ef3fff223d5980a999f92326cbb749790f8c9334
lea32 <shaman@uitk.ru>
2017-10-06 18:20:41 +0500

    *Serving static files

--- src/main/java/ru/leasoft/challenge/aggregator/container/Container.java
+++ src/main/java/ru/leasoft/challenge/aggregator/container/Container.java
@@ -1,6 +1,10 @@
 package ru.leasoft.challenge.aggregator.container;
 
+import io.undertow.Handlers;
 import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.server.handlers.resource.ClassPathResourceManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.leasoft.challenge.aggregator.container.configuration.Configuration;
@@ -12,6 +16,9 @@ public class Container {
     private String host;
     private int port;
 
+    private static final String STATIC_WEB_FILES_PATH = "ru.leasoft.challenge.aggregator.web/";
+    private static final String WELCOME = "index.html";
+
     private static final Logger log = LoggerFactory.getLogger(Container.class);
 
     private Container(String host, int port) {
@@ -20,12 +27,20 @@ public class Container {
 
         server = Undertow.builder()
                 .addHttpListener(port, host)
+                .setHandler(makeStaticWebFilesHandler())
                 .build();
+    }
+
+    private HttpHandler makeStaticWebFilesHandler() {
+        ClassPathResourceManager staticWebFiles = new ClassPathResourceManager(
+                Container.class.getClassLoader(),
+                STATIC_WEB_FILES_PATH
+        );
+        return Handlers.resource(staticWebFiles).addWelcomeFiles(WELCOME);
     }
 
     public static Container buildContainer() {
         Configuration config = Configuration.getInstance();
-
         return new Container(
                 config.getWebserverHost(),
                 config.getWebserverPort()
